cd54a838d500325948925049af5ff23afec2b0a4
Mateo-156w <97768910+Mateo-156w@users.noreply.github.com>
2022-01-14 23:06:18 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import streamlit as st
-from pagina_de_depresion import mostrar_pagina_de_Depresion
+from pagina_de_depresion import mostrar_pagina_de_Depresión
 from pagina_de_bipolaridad import Mostrar_pagina_de_Bipolaridad
 from pagina_principal import mostrar_pagina_principal
 import pickle
@@ -13,7 +13,7 @@ Navigate = st.sidebar.selectbox("Navigate",("Home","Depresión","Bipolaridad"))
 Pagina = Navigate
 
 if Pagina == "Depresión":
-    mostrar_pagina_de_Depresion()
+    mostrar_pagina_de_Depresión()
 if Pagina == "Bipolaridad":
     Mostrar_pagina_de_Bipolaridad()
 if Pagina == "Home":
